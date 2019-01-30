30fce56a240b18702c75f32ea3042c1f0457c00a
Tanner Galloway <tannergalloway15@gmail.com>
2019-01-29 22:49:26 -0700

    integrated omdb into webpage

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,10 +10,11 @@ $(document).ready(function()
     var paragraphTitle;
     var currentSearch;
     var gifDiv;
-    var emptyTextTimer = setInterval(TextCheck, 500); //timer runs ever half second to  disable button if no search term is made or checkboxes are clicked
+    setInterval(TextCheck, 500); //timer runs ever half second to  disable button if no search term is made or checkboxes are clicked
     var searchTerm;
     var form = document.getElementById("gifControler");
-    var favorites = document.getElementById("favorites");
+    // var favorites = document.getElementById("favorites");
+    // var favSelected = false;
 
     //show gifs function
     function showgifs()
@@ -25,12 +26,12 @@ $(document).ready(function()
         {
             currentSearch = search;
             limit = 10;
-            var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&limit=10&api_key=rawxZ3PxzVxAGd3o7vfgr2yqNKtOtArY";
+            var GifqueryURL = "https://api.giphy.com/v1/gifs/search?q=" + search + "&limit=10&api_key=rawxZ3PxzVxAGd3o7vfgr2yqNKtOtArY";
 
         //send ajax call to giphy
         $.ajax(
         {
-            url: queryURL,
+            url: GifqueryURL,
             method: "GET"
         }).then(function(responce)
         {
@@ -48,22 +49,42 @@ $(document).ready(function()
                 var gifTitle = responce.data[i].title;
             
                 //create a div for the p tag and the image add the div to the body
-                gifDiv = $("<div>");
-                paragraphRating = $("<p>");
-                paragraphTitle = $("<p>");
-                var gif = $("<img>");
-                gifDiv.addClass("gifs");
-                paragraphTitle.addClass("hideTitle");
-                paragraphRating.addClass("hideRating");
+                gifDiv = $("<div>").addClass("gifs");
+                
+                //create a check box
+                var checkoxDivFav = $("<div>").addClass("form-check"); 
+                var checkboxFav = $("<input>").addClass("form-check-input");
+                checkboxFav.attr("type", "checkbox");
+                checkboxFav.attr("id", "favCheckbox_" + i);
+                
+                //add label to check box
+                var favlabel = $("<label>").addClass("form-check-label");
+                favlabel.attr("for", "favCheckbox_" + i);
+                favlabel.text("Favorite");
+
+                //create paragraph below image
+                paragraphRating = $("<p>").addClass("hideRating");
+                paragraphTitle = $("<p>").addClass("hideTitle");
                 $(paragraphTitle).text(gifTitle);
                 $(paragraphRating).text("Rating: " + gifRating);
+
+                //create imaage
+                var gif = $("<img>");
+                
+                //make image the gif
                 gif.attr("src", stillGif);
                 gif.attr("value", 0);
                 gif.attr("id", i);
+
+                //display to screen/append to the html 
                 $(".col").append(gifDiv);
                 $(gifDiv).append(gif);
+                $(gifDiv).append(checkoxDivFav);
+                $(checkoxDivFav).append(checkboxFav);
+                $(checkoxDivFav).append(favlabel);
                 $(gifDiv).append(paragraphTitle);
                 $(gifDiv).append(paragraphRating);
+                
 
             }   
         });
@@ -74,44 +95,62 @@ $(document).ready(function()
         limit += 10;
 
         //update queryurl with new limit
-        var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + currentSearch + "&limit=" + limit + "&api_key=rawxZ3PxzVxAGd3o7vfgr2yqNKtOtArY";
+        var GifqueryURL = "https://api.giphy.com/v1/gifs/search?q=" + currentSearch + "&limit=" + limit + "&api_key=rawxZ3PxzVxAGd3o7vfgr2yqNKtOtArY";
 
         //send ajax call to giphy
         $.ajax(
             {
-                url: queryURL,
+                url: GifqueryURL,
                 method: "GET"
             }).then(function(responce)
             {
                 gifData = responce;
-                //empty gif div to not have duplicates
-                // $(".gifs").empty();
             
                 //loop through the returned array
                 for(i = limit-10; i < responce.data.length; i++)
                 {
-                    //get the rating of the gif and the still image of the gif
-                    var rating = responce.data[i].rating;
+                   //get the rating of the gif and the still image of the gif
+                    var gifRating = responce.data[i].rating;
                     var stillGif = responce.data[i].images.original_still.url;
-                    var title = responce.data[i].title;
+                    var gifTitle = responce.data[i].title;
                 
                     //create a div for the p tag and the image add the div to the body
-                    gifDiv = $("<div>");
-                    paragraphRating = $("<p>");
-                    paragraphTitle = $("<p>");
+                    gifDiv = $("<div>").addClass("gifs");
+                
+                    //create a check box
+                    var checkoxDivFav = $("<div>").addClass("form-check");
+                    var checkboxFav = $("<input>").addClass("form-check-input");
+                    checkboxFav.attr("type", "checkbox");
+                    checkboxFav.attr("id", "box" + i);
+                    checkboxFav.attr("id", "favCheckbox_" + i);
+                
+                    //add label to check box
+                    var favlabel = $("<label>").addClass("form-check-label");
+                    favlabel.attr("for", "favCheckbox_" + i);
+                    favlabel.text("Favorite");
+
+                    //create paragraph below image
+                    paragraphRating = $("<p>").addClass("hideRating");
+                    paragraphTitle = $("<p>").addClass("hideTitle");
+                    $(paragraphTitle).text(gifTitle);
+                    $(paragraphRating).text("Rating: " + gifRating);
+
+                    //create imaage
                     var gif = $("<img>");
-                    gifDiv.addClass("gifs");
-                    paragraphTitle.addClass("hideTitle");
-                    paragraphRating.addClass("hideRating");
-                    $(paragraphTitle).text(title);
-                    $(paragraphRating).text("Rating: " + rating);
+                
+                    //make image the gif
                     gif.attr("src", stillGif);
                     gif.attr("value", 0);
                     gif.attr("id", i);
+
+                    //display to screen/appedn to the html 
                     $(".col").append(gifDiv);
                     $(gifDiv).append(gif);
-                    $(gifDiv).append(paragraphRating);
+                    $(gifDiv).append(checkoxDivFav);
+                    $(checkoxDivFav).append(checkboxFav);
+                    $(checkoxDivFav).append(favlabel);
                     $(gifDiv).append(paragraphTitle);
+                    $(gifDiv).append(paragraphRating);
     
                 }   
             });
@@ -178,20 +217,22 @@ function TextCheck()
     //check if search term is empty
     if(searchTerm == "")
     {
+        //disable button
         $("#add-gif").attr("disabled", true);
     }
     else
-    {
+    {   //enable button
         $("#add-gif").attr("disabled", false);
     }
 
     //check if one of the checkboxes is checked
     if($("#gifRating").is(":checked") || $("#gifTitle").is(":checked"))
     {
+        //disable button
         $("#addMoreGifs").attr("disabled", true);
     }
-    else
-    {
+    else 
+    {   //enable button
         $("#addMoreGifs").attr("disabled", false);
     }
 }
@@ -231,10 +272,12 @@ $(document).on("click", ".form-check-input", function()
     {
         //change class
         $(".hideRating").attr("class", "showRating");
+        return;
     }
     else
     {
         $(".showRating").attr("class", "hideRating");
+        
     }
 
     //check if title check box was clicked
@@ -242,48 +285,137 @@ $(document).on("click", ".form-check-input", function()
     {
         //change class
         $(".hideTitle").attr("class", "showTitle");
+        return;
     }
     else
     {
-        $(".showTitle").attr("class", "hideTitle");
+        $(".showTitle").attr("class", "hideTitle");   
     }
+
+    //if movie checkbox id=s clicked
+    if($("#movieCheck").is(":checked"))
+    {
+        //send a query ajax call to omdb api
+        var movieQueryURL = "https://www.omdbapi.com/?t=" + currentSearch + "&y=&plot=short&apikey=trilogy";
+        $.ajax({
+            url: movieQueryURL,
+            method: "GET"
+        }).then(function(responce)
+        {
+            console.log(responce);
+            $("#movieInfo").empty();
+            var movieTitle = $("<h4>");
+            movieTitle.attr("id", "movieTitle");
+            movieTitle.text("Title: " + responce.Title);
+            
+            var movieDate = $("<p>");
+            movieDate.attr("id", "movieDate");
+            movieDate.text("Release Date: " + responce.Released);
+
+            var movieGenre = $("<p>");
+            movieGenre.attr("id", "movieGenre");
+            movieGenre.text("Genre: " + responce.Genre);
+
+            var movieRating = $("<p>");
+            movieRating.attr("id", "movieRating");
+            movieRating.text("Rated: " + responce.Rated);
+
+            var moviePlot = $("<p>");
+            moviePlot.attr("id", "moviePlot");
+            moviePlot.text("Plot: " + responce.Plot);
+
+            var moviePoster = $("<img>");
+            moviePoster.attr("id", "moviePoster");
+            moviePoster.attr("src", responce.Poster);
+
+            //display movie
+            $("#movieInfo").append(movieTitle);
+            $("#movieInfo").append(movieDate);
+            $("#movieInfo").append(movieGenre);
+            $("#movieInfo").append(movieRating);
+            $("#movieInfo").append(moviePlot);
+            $("#movieInfo").append(moviePoster);
+            
+
+        });
+
+    }
+    
+
+    //     //get id of check box that was clicked
+    //     var checkboxId = $(this).attr("id");
+
+    //     //check if the checkbox that was click was a favorite checkbox
+    // for(i = 0; i < gifDiv.data.length; i++)
+    // {
+    //     if(checkboxId == "favCheckbox_" + i && $("#favCheckbox_" + i).is(":checked"))
+    //     {
+    //         favSelected = true;
+    //     }
+    //     else
+    //     {
+    //         favSelected = false;
+    //     }
+    // }
+
+    // if(favSelected)
+    // {
+    // var favGifNum = 0;
+    // //get end of id of checkbox
+    // var boxId = $(this).attr("id").split("_");
+    
+    // //get src of the image associated with the checkbox
+    // var favGifSrc = $("#" + boxId[1]).attr("src");
+
+    // var favGif = $("<img>").addClass("favGifs");
+    // favGif.attr("src", favGifSrc);
+    // favGif.attr("id", "favGif" + favGifNum);
+    // favGifNum++;
+    // $("#favorites").append(favGif);
+    // }
+    // else if(!favSelected)
+    // {
+    //     console.log("hi");
+    // }
 
 
 });
 
-//check if favorites button is clicked
-$("#favoritesButton").on("click", hide_showForm);
+//check if favorites  or movie button is clicked
+// $("#favoritesButton").on("click", hide_showForm);
+$("#movieButton").on("click", hide_showForm);
 
 //hide/show form
 function hide_showForm()
 {
-    if($("#favoritesButton").attr("value") == 0)
+    if($("#movieButton").attr("value") == 0)
     {
         form.style.display = "none";
-        $("#favoritesButton").attr("value", 1);
-        hide_showFavorites();
-        $("#favoritesButton").text("Search");
+        $("#movieButton").attr("value", 1);
+        hide_showmovie();
+        $("#movieButton").text("Search");
     }
-    else if($("#favoritesButton").attr("value") == 1)
+    else if($("#movieButton").attr("value") == 1)
     {
         form.style.display = "block";
-        $("#favoritesButton").attr("value", 0);
-        hide_showFavorites();
-        $("#favoritesButton").text("Favorites");
+        $("#movieButton").attr("value", 0);
+        hide_showmovie();
+        $("#movieButton").text("MovieInfo");
     }
 }
 
-function hide_showFavorites()
+function hide_showmovie()
 {
-    if($("#favoritesButton").attr("value") == 0)
+    if($("#movieButton").attr("value") == 0)
     {
-        favorites.style.display = "none";
+        movieInfo.style.display = "none";
     }
-    else if($("#favoritesButton").attr("value") == 1)
+    else if($("#movieButton").attr("value") == 1)
     {
-        favorites.style.display = "block";
+        movieInfo.style.display = "block";
     }
 }
+
 
 renderButtons();
 });
